37e29ede70c21e30c9c2a50666a241a4d5530449
Aljosa Janjic <janjic_aljosa@hotmail.com>
2023-10-26 23:29:17 +0200

    Adding unlockvideosapp v1

---
+++ Dockerfile
@@ -0,0 +1,21 @@
+# Use an official Python runtime as a parent image
+FROM python:latest
+
+# Set the working directory in the container
+WORKDIR /app
+
+# Create the /app/output directory and set its permissions
+VOLUME /app/output
+
+# Copy your code and video file into the container
+COPY Test_videos/Video_test_1.mp4 .
+COPY main.py .
+
+# Install required Python packages for your application
+RUN pip install --no-cache-dir ffmpeg-python moviepy SpeechRecognition nltk
+
+# Install ffmpeg in the container
+RUN apt-get update && apt-get install -y ffmpeg flac
+
+# Run your script when the container launches
+CMD ["python", "main.py"]
---
+++ main.py
@@ -0,0 +1,46 @@
+# import os
+# os.environ["IMAGEIO_FFMPEG_EXE"] = "/usr/bin/ffmpeg"
+import ffmpeg
+from moviepy.editor import VideoFileClip
+from speech_recognition import Recognizer, AudioFile
+import nltk
+from nltk.sentiment import SentimentIntensityAnalyzer
+
+recognizer = Recognizer()
+
+audio_file_location = '/app/output/Video_test_1.wav'
+
+video = VideoFileClip("Video_test_1.mp4")
+video.audio.write_audiofile(audio_file_location)
+
+with AudioFile(audio_file_location) as audio_file:
+  audio = recognizer.record(audio_file)
+
+text = recognizer.recognize_google(audio)
+
+# The filename of the file you want to write to
+#filename = f'video_transcript.txt'
+
+filename = '/app/output/video_transcript.txt'
+
+
+# Open the file with writing permission
+with open(filename, 'w') as file:
+  # Write text to the file
+  file.write(text)
+
+print(text)
+
+
+
+# Analyzer of speech sentiment
+# nltk.download('vader_lexicon')
+
+# analyzer = SentimentIntensityAnalyzer()
+# scores = analyzer.polarity_scores(text)
+# print(scores)
+
+# if scores['compound'] > 0:
+#   print('Positive Speech')
+# else:
+#   print('Negative Speech')
