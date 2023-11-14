bf426054051c463325fec138e0fe7c08709ffc21
Aljosa Janjic <janjic_aljosa@hotmail.com>
2023-11-14 08:48:57 +0100

    Playing with json file

--- jsonapi/myapi/views.py
+++ jsonapi/myapi/views.py
@@ -4,4 +4,7 @@ from django.http import JsonResponse
 # Create your views here.
 
 def spolocna_praca_funguje(request):
-    return JsonResponse({"message": "Diky kamo za pomoc vcera, toto teraz bezi na Django development servery :) "})
+    return JsonResponse({
+        "message": "Diky kamo za pomoc vcera, toto teraz bezi na Django development servery :)",
+        "option": "Y"
+        })
