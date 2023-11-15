c3aed21a9449c4fe996e35bfa2bd2c4873a14786
Aljosa Janjic <janjic_aljosa@hotmail.com>
2023-11-15 09:43:08 +0100

    Creating a template for interaction with DB

--- jsonapi/myapi/urls.py
+++ jsonapi/myapi/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('api/dummydata', views.spolocna_praca_funguje, name='dummy_data'),
+    path('dummydata', views.spolocna_praca_funguje, name='dummy_data'),
     path('add/', views.add_transcript, name='add_transcript'),
     path('view/', views.view_transcripts, name='view_transcripts'),
     path('delete/<int:id>/', views.delete_transcript, name='delete_transcript'),
